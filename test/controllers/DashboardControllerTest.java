da8b8c456894514a453225710580b2d5c046cbfa
buleron <buleron>
2019-10-29 17:28:59 +0100

    add dashboards CRUD test(onebyone)mode

--- test/controllers/DashboardControllerTest.java
+++ test/controllers/DashboardControllerTest.java
@@ -2,8 +2,10 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.BasicDBObject;
 import models.collection.Dashboard;
 import org.apache.http.Header;
+import org.bson.types.ObjectId;
 import play.test.WithApplication;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -42,17 +44,6 @@ public class DashboardControllerTest extends WithApplication {
         return dataNode;
     }
 
-//    public static String BuildResult(Result result) {
-//        String resultStr = play.test.Helpers.contentAsString(result);
-//        if(!resultStr.contains("ERR_AUTHORIZATION_HEADER")){
-//            ObjectNode json = (ObjectNode) Json.parse(resultStr);
-//            System.out.println("RESPONSE: " + json.toString());
-//            String token = json.get("token").asText();
-//            return token;
-//        } else {
-//            return resultStr;
-//        }
-//    }
 
     @Test
     public void createDashboard_OK() throws IOException {
@@ -74,7 +65,6 @@ public class DashboardControllerTest extends WithApplication {
         System.out.println(dashboard);
         assertEquals(OK, result.status());
     }
-
     @Test
     public void createDashboard_MissingToken() {
         ObjectNode dataNode = Json.newObject();
@@ -99,7 +89,6 @@ public class DashboardControllerTest extends WithApplication {
         System.out.println(resultStr);
         assertEquals(BAD_REQUEST, result.status());
     }
-
     @Test
     public void createDashboard_WrongToken() {
         ObjectNode dataNode = Json.newObject();
@@ -114,26 +103,76 @@ public class DashboardControllerTest extends WithApplication {
         assertEquals(UNAUTHORIZED, result.status());
     }
 
+
     @Test
-    public void updateDashboard() {
+    public void updateDashboard_BadRequest() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(PUT)
+                .header("Content-Type","application/json")
+                .header(AUTHORIZATION,BEARER_Token)
+                .uri(AuthURL);
+        Result result = route(app, request);
+        assertEquals(BAD_REQUEST, result.status());
+    }
+    @Test
+    public void updateDashboard_WrongToken() {
         ObjectNode dataNode = Json.newObject();
         dataNode.put("name", "UPDATED FROM TEST");
-        dataNode.put("description", "UPDATED From tests");
+        dataNode.put("description", "UPDATED From tests yeap updated again");
         dataNode.put("parentId", "5dada6f7ee5cd920804cdb31");
+        dataNode.put("_id","5dada6f7ee5cd920804cdb31");
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(PUT)
+                .header("Content-Type","application/json")
+                .header(AUTHORIZATION,BEARER+WrongToken)
+                .bodyJson(dataNode)
+                .uri(AuthURL);
+        Result result = route(app, request);
+        assertEquals(UNAUTHORIZED, result.status());
+    }
+    @Test
+    public void updateDashboard_OK() {
+        ObjectNode dataNode = Json.newObject();
+        dataNode.put("name", "UPDATED FROM TEST");
+        dataNode.put("description", "UPDATED From tests yeap updated again");
+        dataNode.put("parentId", "5dada6f7ee5cd920804cdb31");
+        dataNode.put("_id","5dada6f7ee5cd920804cdb31");
         Http.RequestBuilder request = new Http.RequestBuilder()
                 .method(PUT)
                 .header("Content-Type","application/json")
                 .header(AUTHORIZATION,BEARER_Token)
                 .bodyJson(dataNode)
-                .uri(AuthURL+"5dada6f7ee5cd920804cdb31");
+                .uri(AuthURL);
         Result result = route(app, request);
         String resultStr = play.test.Helpers.contentAsString(result);
         System.out.println(resultStr);
         assertEquals(OK, result.status());
     }
 
+
     @Test
-    public void getDashboardById() {
+    public void getDashboardById_BadRequest() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .header("Content-Type","application/json")
+                .uri(AuthURL+"5dada6f7ee5cd920804cdb31");
+        Result result = route(app, request);
+        assertEquals(FORBIDDEN, result.status());
+    }
+    @Test
+    public void getDashboardById_WrongToken() {
+        ObjectNode dataNode = Json.newObject();
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .header("Content-Type","application/json")
+                .header(AUTHORIZATION,BEARER+WrongToken)
+                .bodyJson(dataNode)
+                .uri(AuthURL+"5dada6f7ee5cd920804cdb31");
+        Result result = route(app, request);
+        assertEquals(UNAUTHORIZED, result.status());
+    }
+    @Test
+    public void getDashboardById_OK() {
         ObjectNode dataNode = Json.newObject();
         Http.RequestBuilder request = new Http.RequestBuilder()
                 .method(GET)
@@ -146,6 +185,7 @@ public class DashboardControllerTest extends WithApplication {
         System.out.println(resultStr);
         assertEquals(OK, result.status());
     }
+
 
     @Test
     public void dashboardGetAll() {
@@ -162,9 +202,8 @@ public class DashboardControllerTest extends WithApplication {
         System.out.println(resultStr);
         assertEquals(OK, result.status());
     }
-
     @Test
-    public void deleteDashboard() {
+    public void deleteDashboard_BadRequest() {
         ObjectNode dataNode = Json.newObject();
         dataNode.put("_id", "amassed");
         Http.RequestBuilder request = new Http.RequestBuilder()
@@ -173,6 +212,51 @@ public class DashboardControllerTest extends WithApplication {
                 .header(AUTHORIZATION,BEARER_Token)
                 .uri(AuthURL);
         Result result = route(app, request);
-        assertEquals(UNAUTHORIZED, result.status());
+        assertEquals(FORBIDDEN, result.status());
+    }
+    @Test
+    public void deleteDashboard_WrongToken() {
+        ObjectNode dataNode = Json.newObject();
+        dataNode.put("_id", "5dada734ee5cd920804cdb32");
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(DELETE)
+                .bodyJson(dataNode)
+                .header(AUTHORIZATION,BEARER+WrongToken)
+                .uri(AuthURL);
+        Result result = route(app, request);
+        assertEquals(FORBIDDEN, result.status());
+    }
+    @Test
+    public void deleteDashboard_NoToken() {
+        ObjectNode dataNode = Json.newObject();
+        dataNode.put("_id", "5dada734ee5cd920804cdb32");
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(DELETE)
+                .bodyJson(dataNode)
+                .uri(AuthURL);
+        Result result = route(app, request);
+        assertEquals(FORBIDDEN, result.status());
+    }
+    @Test
+    public void deleteDashboard_NotFound() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(DELETE)
+                .header(AUTHORIZATION,BEARER_Token)
+                .uri(AuthURL+"5dada6f7ee5cd920804cdb31");
+        Result result = route(app, request);
+        String resultStr = play.test.Helpers.contentAsString(result);
+        System.out.println(resultStr);
+        assertEquals(NOT_FOUND, result.status());
+    }
+    @Test
+    public void deleteDashboard_OK() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(DELETE)
+                .header(AUTHORIZATION,BEARER_Token)
+                .uri(AuthURL+"5dada6f7ee5cd920804cdb31");
+        Result result = route(app, request);
+        String resultStr = play.test.Helpers.contentAsString(result);
+        System.out.println(resultStr);
+        assertEquals(OK, result.status());
     }
 }
