cc210eaacc7e51e3ec065cdec8cb5557069170ea
buleron <buleron>
2019-12-03 16:19:54 +0100

    per Arbesen se spo kryhet :P

--- app/models/collection/content/IContent.java
+++ app/models/collection/content/IContent.java
@@ -11,7 +11,10 @@ import models.enums.ContentType;
         @JsonSubTypes.Type(value= EmailContent.class, name = "EMAIL"),
         @JsonSubTypes.Type(value= TextContent.class, name = "TEXT"),
         @JsonSubTypes.Type(value= ImageContent.class, name = "IMAGE"),
-        @JsonSubTypes.Type(value= LineContent.class, name = "LINE")
+        @JsonSubTypes.Type(value= LineContent.class, name = "LINE"),
+        @JsonSubTypes.Type(value= LineContent.class, name = "PIE"),
+        @JsonSubTypes.Type(value= LineContent.class, name = "BAR"),
+        @JsonSubTypes.Type(value= LineContent.class, name = "TREE")
 })
 public interface IContent {
     ContentType type = ContentType.TEXT;
