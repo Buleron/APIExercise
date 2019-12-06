947d38cf2438c508d7fb2d4c4fba277eabedbb98
buleron <buleron>
2019-12-06 14:31:44 +0100

    refactor

--- app/services/ContentService.java
+++ app/services/ContentService.java
@@ -2,12 +2,9 @@ package services;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.model.Filters;
 import data.ContentDataAccess;
-import data.DashboardsDataAccess;
 import data.DataAccess;
-import models.collection.Dashboard;
 import models.collection.User;
 import models.collection.content.DashboardContent;
 import models.enums.AccessLevelType;
@@ -21,9 +18,7 @@ import org.bson.types.ObjectId;
 import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Http;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
@@ -40,10 +35,11 @@ public class ContentService {
     SingleThreadedExecutionContext singleThreadedExecutionContext;
 
     public CompletableFuture<DashboardContent> findByDashboardId(String did, User authUser) {
-        MongoRelay relay = new MongoRelay(mongoDB.getDatabase(), authUser).withACL(DashboardContent.class, AccessLevelType.READ);
-        RelayCollection<DashboardContent> dashboardContentRelayCollection = new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(relay).getRelay().getCollection();
-        RelayFindIterable<DashboardContent, DashboardContent> findIterable = dashboardContentRelayCollection.find(DashboardContent.class).filter(Filters.eq("dashboardId", new ObjectId(did)));
-        DashboardContent found = findIterable.first();
+        RelayCollection<DashboardContent> dashboardContentRelayCollection =
+                new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(new MongoRelay(mongoDB.getDatabase(), authUser)
+                        .withACL(DashboardContent.class, AccessLevelType.READ)).getRelay().getCollection();
+
+        DashboardContent found = dashboardContentRelayCollection.find(DashboardContent.class).filter(Filters.eq("dashboardId", new ObjectId(did))).first();
         if (found == null)
             throw new CompletionException(new RequestException(Http.Status.NOT_FOUND, NOT_FOUND));
         return CompletableFuture.completedFuture(found);
@@ -52,10 +48,8 @@ public class ContentService {
     public CompletableFuture<DashboardContent> findById(String did, String x, User authUser) {
         try {
             MongoRelay relay = new MongoRelay(mongoDB.getDatabase(), authUser).withACL(DashboardContent.class, AccessLevelType.READ);
-            if (did == null) {
-                DataAccess<DashboardContent> access = new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(relay);
-                return access.byIdAsync(new ObjectId(x), context.current());
-            }
+            if (did == null)
+                return new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(relay).byIdAsync(new ObjectId(x), context.current());
             RelayCollection<DashboardContent> dashboardContentRelayCollection = new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(relay).getRelay().getCollection();
             RelayFindIterable findIterable = dashboardContentRelayCollection.find()
                     .filter(Filters.and(Filters.eq("_id", new ObjectId(x)), Filters.eq("dashboardId", new ObjectId(did))));
@@ -69,8 +63,6 @@ public class ContentService {
     }
 
     public CompletableFuture<DashboardContent> save(DashboardContent resDashboardContent, User authUser) {
-        MongoRelay relay = new MongoRelay(mongoDB.getDatabase(), authUser).withACL(DashboardContent.class, AccessLevelType.WRITE);
-
         Set<String> generalAccesses = new HashSet<>();
         //todo set access for user that create this :/
         generalAccesses.add(authUser.getId().toString());
@@ -84,20 +76,21 @@ public class ContentService {
 
         resDashboardContent.setReadACL(read);
         resDashboardContent.setWriteACL(write);
-        return new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(relay).insert(resDashboardContent, context.current());
+        return new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(new MongoRelay(mongoDB.getDatabase(), authUser)
+                .withACL(DashboardContent.class, AccessLevelType.WRITE)).insert(resDashboardContent, context.current());
     }
 
     public CompletableFuture<DashboardContent> update(DashboardContent resDashboardContent, User authUser) {
-        MongoRelay relay = new MongoRelay(mongoDB.getDatabase(), authUser).withACL(DashboardContent.class, AccessLevelType.WRITE);
-        return new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(relay).insert(resDashboardContent, context.current());
+        return new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(new MongoRelay(mongoDB.getDatabase(), authUser)
+                .withACL(DashboardContent.class, AccessLevelType.WRITE)).insert(resDashboardContent, context.current());
     }
 
     public CompletableFuture<DashboardContent> delete(String contentID, User authUser) {
         return this.findById(null, contentID, authUser).thenCompose((item) -> {
             if (item == null)
                 throw new CompletionException(new RequestException(Http.Status.NOT_FOUND, NOT_FOUND));
-            MongoRelay relay = new MongoRelay(mongoDB.getDatabase(), authUser).withACL(DashboardContent.class, AccessLevelType.WRITE);
-            return new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(relay).deleteAsync(item, context.current());
+            return new ContentDataAccess(mongoDB.getDatabase()).withMongoRelay(new MongoRelay(mongoDB.getDatabase(), authUser)
+                    .withACL(DashboardContent.class, AccessLevelType.WRITE)).deleteAsync(item, context.current());
         });
     }
 }
